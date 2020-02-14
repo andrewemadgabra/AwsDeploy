e1d81b8742f6753ebc00136be96847d791cb51c5
anderw emad <anderwemad@hotmail.com>
2020-02-14 23:12:24 +0200

    Edit server

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,21 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get("/filteredimage/", async (req, res) => {
+    const url = req.query.image_url as string;
 
+    if (!url) {
+      res.sendStatus(400).send(`image_url`);
+    }
+
+    try {
+      const filteredpath = await filterImageFromURL(url);
+      res.sendFile(filteredpath, () => deleteLocalFiles([filteredpath]));
+    } catch(error) {
+      res.sendStatus(422); 
+    }
+  });
+  
   //! END @TODO1
   
   // Root Endpoint
